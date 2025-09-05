bd5c12aff15a72cbb32ab137623c03981914a48c
LorenzoCorra95 <lorenzo.corra@azero.veneto.it>
2025-09-05 12:01:31 +0200

    app.py

--- app.py
+++ app.py
@@ -232,7 +232,6 @@ if st.button("Esegui analisi"):
         output.seek(0)
         wb = op.load_workbook(output)
         for foglio in wb:
-            foglio.delete_cols(1)
             n_col = foglio.max_column
             foglio.cell(1, n_col+1, value="Nuova Autonomia")
             contaR=1
@@ -268,6 +267,7 @@ if st.button("Esegui analisi"):
             file_name="sottoscorta.xlsx",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
+
 
 
 
